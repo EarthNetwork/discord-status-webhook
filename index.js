b44ffea95238ca64a8d68f1306107ea69b8dc301
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-02-06 14:29:14 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const isDegraded = process.env.issueTitle.includes("has degraded performance")
+const isDown = process.env.issueTitle.includes("is down")
+let params = {
+    embeds: [
+        {
+            "description": process.env.issueTitle.replace("is down", "est down").replace("has degraded performance", "prend plus de temps que d'habitude pour répondre").replace("performance has improved", "a retrouvé la forme").replace("is back up", "est de retour"),
+            "color": IsDown ? 0xed4245 : (isDegraded ? 0xfee75c : 0x57f287),
+		}
+    ]
+}
+fetch(process.env.discordUrl, {
+    method: "POST",
+    headers: {
+        'Content-type': 'application/json'
+    },
+    body: JSON.stringify(params)
+}).then(res => {
+    console.log(res);
+}) 
