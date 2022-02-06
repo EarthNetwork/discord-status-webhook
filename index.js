fe4e5b611765eb369a7632b3678b317007fe18bc
GeekCorner <45696571+GeekCornerGH@users.noreply.github.com>
2022-02-06 14:41:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const isBack = process.env.back
 const isDegraded = process.env.issueTitle.includes("has degraded performance")
 const isDown = process.env.issueTitle.includes("is down")
 let params = {
     embeds: [
         {
             "description": process.env.issueTitle.replace("is down", "est down").replace("has degraded performance", "prend plus de temps que d'habitude pour répondre").replace("performance has improved", "a retrouvé la forme").replace("is back up", "est de retour"),
-            "color": IsDown ? 0xed4245 : (isDegraded ? 0xfee75c : 0x57f287),
+            "color": isBack ? 0x57f287 : (isDegraded ? 0xfee75c :0xed4245),
 		}
     ]
 }
